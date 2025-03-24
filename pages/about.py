24203ac3a2db82448dbe9d1b61692ad326a93a46
Abdul Rehman <ar5254843@gmail.com>
2025-03-24 21:36:47 +0500

    added changes in file

--- pages/about.py
+++ pages/about.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("📖 About Us")
 st.write("Welcome to the About page! Here, we share more details about our website and its purpose.")
+st.caption("Run with command")
+st.code("streamlit run app.py")
